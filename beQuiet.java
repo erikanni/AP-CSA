800f6b11fee7095983e14d0fce81a5b65e1aac68
erikanni <erik.anni@gmail.com>
2021-01-21 19:47:08 -0500

    add main method

--- beQuiet.java
+++ beQuiet.java
@@ -1,45 +1,26 @@
 public class beQuiet {
-    public int amplitudeChecker (int beQuiet){
-
-
+    int beQuiet = 90;
+    public static int amplitudeChecker (int beQuiet){
         int count = 0;
-     
-     
         for (int i = 0; i < what.length; i++){
-     
-     
            if (what[i] > beQuiet){
-     
-     
                what[i] = beQuiet;
-     
-     
                count++;
-     
-     
                }
-     
-     
            else if (what[i] < -beQuiet)
-     
-     
            {
-     
-     
            what[i] = -beQuiet;
-     
-     
            count++;
-     
-     
            }       
-     
-     
         }
-     
-     
      return count;
-     
-     
      }
+    public static void main()
+    {
+        int[] what = new int[6];
+        int what[] = {100, 100, 50, 75, -100, -200};
+        int beQuiet = 90;
+        amplitudeChecker(beQuiet);
+    }
 }
+
