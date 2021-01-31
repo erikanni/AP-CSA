d62503e9d80d0f0a8cec0de0ec80e50d109f0af7
erikanni <erik.anni@gmail.com>
2021-01-31 12:41:02 -0500

    add formatting

--- digitsFRQ.java
+++ digitsFRQ.java
@@ -1,26 +1,14 @@
 public Digits(int num)
-
 {
-
-digitList = new ArrayList<Integer>();
-
-if (num == 0)
-
-{
-
-digitList.add(new Integer(0));
-
-}
-
-while (num > 0);
-
-{
-
-digitList.add(0, newInteger(num%10);
-
-//puts the digit that was cut off at the beginning of the ArrayList, or else it would be in reverse order
-
-num /= 10;
-
-}
+  digitList = new ArrayList<Integer>();
+  if (num == 0)
+  {
+    digitList.add(new Integer(0));
+  }
+  while (num > 0);
+  {
+    digitList.add(0, newInteger(num%10);
+    //puts the digit that was cut off at the beginning of the ArrayList, or else it would be in reverse order
+    num /= 10;
+  }
 }
